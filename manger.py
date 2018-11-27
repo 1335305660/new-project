a5cb51ceee61567d963db44293abbd1e7f1a8cd4
jim <jim@qq.com>
2018-11-27 21:19:37 +0800

    session提交

--- manger.py
+++ manger.py
@@ -1,17 +1,28 @@
-from flask import Flask
+from flask import Flask, Session
 from flask_sqlalchemy import SQLAlchemy
 from redis import StrictRedis
+from flask_wtf import CSRFProtect
+from flask_session import Session
 
 
 class Config(object):
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = "mysql://root:mysql@127.0.0.1:3306/python22"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
+    SECRET_KEY = "ASJFJKSA1465879"
     # redis数据库配置信息
     REDIS_HOST = "127.0.0.1"
     REDIS_PORT = 6379
 
+
+    # 将session 存储的数据从内存转移到ｒｅｄｉｓ中存储的配置信息中去
+    SESSION_TYPE = "redis"
+    SESSION_REDIS = StrictRedis(REDIS_HOST, REDIS_PORT)
+    SESSION_USE_SIGNER = True
+    # 设置数据不需要永久保存，　而是根据我们设置的过期时长进行调整
+    SESSION_PERMANENT = False
+
+    PERMANENT_SESSION_LIFETIME = 86400 # 24小时
 
 
 
@@ -21,6 +32,10 @@ app.config.from_object(Config)
 db = SQLAlchemy(app)
 
 redis_store = StrictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT)
+# 给项目添加防护机制
+CSRFProtect(app)
+
+Session(app)
 
 
 @app.route('/')
