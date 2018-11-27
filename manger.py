988419a0724495a6daf2ff7ed8b02acbc508bdc2
jim <jim@qq.com>
2018-11-27 19:35:17 +0800

    redis提交

--- manger.py
+++ manger.py
@@ -1,7 +1,26 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from redis import StrictRedis
+
+
+class Config(object):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = "mysql://root:mysql@127.0.0.1:3306/python22"
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+
+    # redis数据库配置信息
+    REDIS_HOST = "127.0.0.1"
+    REDIS_PORT = 6379
+
+
+
 
 
 app = Flask(__name__)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+
+redis_store = StrictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT)
 
 
 @app.route('/')
@@ -10,4 +29,5 @@ def index():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    print(app.url_map)
+    #app.run(debug=True)
